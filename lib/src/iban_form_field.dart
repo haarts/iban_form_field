144128a8ec6d0449d4b49ea0ce3cc2c7da4a1ca1
Harm Aarts <harm@aarts.email>
2019-09-10 14:05:06 +0200

    Extract widget, move to next/previous field
    The builder lambda became stupidly large.

--- lib/src/iban_form_field.dart
+++ lib/src/iban_form_field.dart
@@ -1,103 +1,219 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:iban_form_field/src/iban_model.dart';
+import 'package:iban_form_field/src/input_formatters/iban_paste_input_formatter.dart';
+import 'package:iban_form_field/src/input_formatters/spaced_text_input_formatter.dart';
 
 class IbanFormField extends FormField<Iban> {
   IbanFormField({
+    Key key,
     FormFieldSetter<Iban> onSaved,
     FormFieldValidator<Iban> validator,
     Iban initialValue,
-    bool autovalidate = false,
   }) : super(
-            onSaved: onSaved,
-            validator: validator,
-            initialValue: initialValue,
-            autovalidate: autovalidate,
-            builder: (FormFieldState<Iban> state) {
-              return Row(
-                children: [
-                  Container(
-                    constraints: BoxConstraints.tightFor(
-                      width: Theme.of(state.context).textTheme.body1.fontSize *
-                          1.1 *
-                          2,
-                    ),
-                    padding: EdgeInsets.only(right: 5),
-                    child: TextFormField(
-                      initialValue: initialValue.countryCode,
-                      maxLength: 2,
-                      textCapitalization: TextCapitalization.characters,
-                      inputFormatters: [
-                        WhitelistingTextInputFormatter(RegExp("[A-Z]")),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    constraints: BoxConstraints.tightFor(
-                      width: Theme.of(state.context).textTheme.body1.fontSize *
-                          1.1 *
-                          2,
-                    ),
-                    padding: EdgeInsets.only(right: 5),
-                    child: TextFormField(
-                      initialValue: initialValue.checkDigits,
-                      maxLength: 2,
-                      inputFormatters: [
-                        WhitelistingTextInputFormatter(RegExp("[0-9]")),
-                      ],
-                    ),
-                  ),
-                  Flexible(
-                    child: TextFormField(
-                      initialValue: initialValue.basicBankAccountNumber,
-                      maxLength: initialValue.maxBasicBankAccountNumberLength,
-                      textCapitalization: TextCapitalization.characters,
-                      inputFormatters: [
-                        SpacedTextInputFormatter(),
-                      ]
-                    ),
-                  ),
-                ],
-              );
-            });
+          key: key,
+          onSaved: onSaved,
+          validator: validator,
+          initialValue: initialValue ?? Iban('CH'),
+          builder: (FormFieldState<Iban> state) {
+            return IbanFormFieldBuilder(state);
+          },
+        );
 }
 
-class IbanCountryCode extends FormField<String> {}
+class IbanFormFieldBuilder extends StatefulWidget {
+  final FormFieldState<Iban> state;
+  IbanFormFieldBuilder(this.state);
 
-class SpacedTextInputFormatter extends TextInputFormatter {
   @override
-  TextEditingValue formatEditUpdate(
-    TextEditingValue oldValue,
-    TextEditingValue newValue,
-  ) {
-    String splitText = _splitInGroupsOfFour(newValue.text);
-    int numberOfSpacesAdded = splitText.length - newValue.text.length;
+  State createState() => _IbanFormFieldState();
+}
 
-    return TextEditingValue(
-      text: splitText,
-      selection: TextSelection.collapsed(
-        offset: newValue.selection.baseOffset + numberOfSpacesAdded,
-      ),
+class _IbanFormFieldState extends State<IbanFormFieldBuilder> {
+  static const _maxCountryCodeLength = 2;
+  static const _maxCheckDigitLength = 2;
+
+  final TextEditingController _controllerCountryCode = TextEditingController();
+  final TextEditingController _controllerCheckDigits = TextEditingController();
+  final TextEditingController _controllerBasicBankAccountNumber =
+      TextEditingController();
+
+  // Used for jumping to next/previous field
+  final FocusNode _focusCountryCode = FocusNode();
+  final FocusNode _focusCheckDigits =
+      FocusNode(debugLabel: 'check-digits-focus-node');
+  final FocusNode _focusBasicBankAccountNumber = FocusNode();
+
+  // Used to determine if the user added or deleted characters
+  int _previousCountryCodeLength;
+  int _previousCheckDigitsLength;
+  int _previousBasicBankAccountNumberLength;
+
+  bool _lastCountryCodeCharacterAdded(int currentLength, int previousLength) {
+    return currentLength == _maxCountryCodeLength &&
+        currentLength - previousLength == 1;
+  }
+
+  bool _lastCheckDigitCharacterAdded(int currentLength, int previousLength) {
+    return currentLength == _maxCheckDigitLength &&
+        currentLength - previousLength == 1;
+  }
+
+  bool _lastCheckDigitCharacterDeleted(int currentLength, int previousLength) {
+    return currentLength == 0 && currentLength - previousLength == -1;
+  }
+
+  bool _lastBasicBankAccountNumberCharacterDeleted(
+      int currentLength, int previousLength) {
+    return currentLength == 0 && currentLength - previousLength == -1;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controllerCountryCode.text = widget.state.value.countryCode;
+    _controllerCheckDigits.text = widget.state.value.checkDigits;
+    _controllerBasicBankAccountNumber.text =
+        widget.state.value.basicBankAccountNumber;
+
+    _previousCountryCodeLength = widget.state.value.countryCode?.length ?? 0;
+    _previousCheckDigitsLength = widget.state.value.checkDigits?.length ?? 0;
+    _previousBasicBankAccountNumberLength =
+        widget.state.value.basicBankAccountNumber?.length ?? 0;
+
+    _controllerCountryCode.addListener(() {
+      if (_lastCountryCodeCharacterAdded(
+          _controllerCountryCode.text.length, _previousCountryCodeLength)) {
+        setState(() {
+          widget.state.value.countryCode = _controllerCountryCode.text;
+        });
+        _focusCountryCode.nextFocus();
+      }
+
+      _previousCountryCodeLength = _controllerCountryCode.text.length;
+    });
+
+    _controllerCheckDigits.addListener(() {
+      if (_lastCheckDigitCharacterDeleted(
+          _controllerCheckDigits.text.length, _previousCheckDigitsLength)) {
+        _focusCheckDigits.previousFocus();
+      }
+
+      if (_lastCheckDigitCharacterAdded(
+          _controllerCheckDigits.text.length, _previousCheckDigitsLength)) {
+        _focusCheckDigits.nextFocus();
+      }
+
+      _previousCheckDigitsLength = _controllerCheckDigits.text.length;
+    });
+
+    _controllerBasicBankAccountNumber.addListener(() {
+      if (_lastBasicBankAccountNumberCharacterDeleted(
+          _controllerBasicBankAccountNumber.text.length,
+          _previousBasicBankAccountNumberLength)) {
+        _focusBasicBankAccountNumber.previousFocus();
+      }
+
+      _previousBasicBankAccountNumberLength =
+          _controllerBasicBankAccountNumber.text.length;
+    });
+  }
+
+  @override
+  void dispose() {
+    _controllerCountryCode.dispose();
+    _controllerCheckDigits.dispose();
+    _controllerBasicBankAccountNumber.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          constraints: BoxConstraints.tightFor(
+            width: Theme.of(widget.state.context).textTheme.body1.fontSize *
+                1.1 *
+                2,
+          ),
+          padding: EdgeInsets.only(right: 5),
+          child: TextFormField(
+            key: Key('country-code'),
+            controller: _controllerCountryCode,
+            focusNode: _focusCountryCode,
+            decoration: const InputDecoration(
+              hintText: 'CH',
+            ),
+            textInputAction: TextInputAction.next,
+            onSaved: (countryCode) {
+              widget.state.value.countryCode = countryCode;
+            },
+            maxLength: 2,
+            textCapitalization: TextCapitalization.characters,
+            inputFormatters: [
+              IbanPasteInputFormatter.countryCode(
+                _controllerCheckDigits,
+                _controllerBasicBankAccountNumber,
+              ),
+              WhitelistingTextInputFormatter(RegExp("[A-Z]")),
+            ],
+          ),
+        ),
+        Container(
+          constraints: BoxConstraints.tightFor(
+            width: Theme.of(widget.state.context).textTheme.body1.fontSize *
+                1.1 *
+                2,
+          ),
+          padding: EdgeInsets.only(right: 5),
+          child: TextFormField(
+            controller: _controllerCheckDigits,
+            focusNode: _focusCheckDigits,
+            decoration: const InputDecoration(
+              hintText: '12',
+            ),
+            textInputAction: TextInputAction.next,
+            onSaved: (checkDigits) {
+              widget.state.value.checkDigits = checkDigits;
+            },
+            maxLength: 2,
+            keyboardType: TextInputType.numberWithOptions(),
+            inputFormatters: [
+              IbanPasteInputFormatter.checkDigits(
+                _controllerCountryCode,
+                _controllerBasicBankAccountNumber,
+              ),
+              WhitelistingTextInputFormatter(RegExp("[0-9]")),
+            ],
+          ),
+        ),
+        Flexible(
+          child: TextFormField(
+            controller: _controllerBasicBankAccountNumber,
+            focusNode: _focusBasicBankAccountNumber,
+            decoration: InputDecoration(
+              hintText: widget.state.value.hintText,
+            ),
+            onSaved: (basicBankAccountNumber) {
+              widget.state.value.basicBankAccountNumber =
+                  basicBankAccountNumber;
+            },
+            maxLength: widget.state.value.maxBasicBankAccountNumberLength,
+            textCapitalization: TextCapitalization.characters,
+            inputFormatters: [
+              IbanPasteInputFormatter.basicBankAccountNumber(
+                _controllerCountryCode,
+                _controllerCheckDigits,
+              ),
+              SpacedTextInputFormatter(),
+            ],
+          ),
+        ),
+      ],
     );
   }
-
-  String _splitInGroupsOfFour(String input) {
-    input = input.replaceAll(RegExp(" "), "");
-    List<String> segments = input.split('');
-    int groupSize = 4;
-    var added = 0;
-    String result = '';
-    var it = segments.iterator;
-    while(it.moveNext()) {
-      if (added != 0 && added % groupSize == 0) {
-        result += ' ';
-      }
-      added++;
-      result += it.current;
-    }
-    return result;
-  }
 }
-
