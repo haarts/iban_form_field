851f6252f337634f96f5822025a622ce618f9933
Dominik Spicher <dominik.spicher@inacta.ch>
2020-05-12 13:16:31 +0200

    fmt

--- test/iban_model_test.dart
+++ test/iban_model_test.dart
@@ -4,19 +4,19 @@ import 'package:iban_form_field/iban_form_field.dart';
 
 void main() {
   test('electronicFormat', () {
-		var iban = Iban('NL')
-  		..checkDigits = '80'
-  		..basicBankAccountNumber = 'INGB 7569 0064 53';
+    var iban = Iban('NL')
+      ..checkDigits = '80'
+      ..basicBankAccountNumber = 'INGB 7569 0064 53';
 
-		expect(iban.electronicFormat, 'NL80INGB7569006453');
+    expect(iban.electronicFormat, 'NL80INGB7569006453');
   });
 
   test('toPrintFormat', () {
-		var iban = Iban('NL')
-  		..checkDigits = '80'
-  		..basicBankAccountNumber = 'INGB7569006453';
+    var iban = Iban('NL')
+      ..checkDigits = '80'
+      ..basicBankAccountNumber = 'INGB7569006453';
 
-		expect(iban.toPrintFormat, 'NL80 INGB 7569 0064 53');
+    expect(iban.toPrintFormat, 'NL80 INGB 7569 0064 53');
   });
 
   test('hintText without a valid countryCode', () {
